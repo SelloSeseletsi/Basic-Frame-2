f62e0a689c416fae24b7e2b3edd599d9c2e3f3b5
SelloSeseletsi <141185307+SelloSeseletsi@users.noreply.github.com>
2023-08-01 23:29:02 +0200

    Add files via upload

---
+++ GUIIntro.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guiintro;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author SELLO
+ */
+public class GUIIntro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        MyFrame frame2 = new MyFrame();
+       
+        
+    }
+    
+}
---
+++ MyFrame.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guiintro;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author SELLO
+ */
+public class MyFrame extends JFrame{
+
+    public MyFrame() {
+        setTitle("Sello Seseletsi");
+        setSize(360,240);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+    
+    
+}
